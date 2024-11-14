dea6486f2e27326b913dece30157a1dd9d7b4dc9
Jay-Shyam <jayshyamgowda12@gmail.com>
2024-11-14 23:59:53 +0530

    Update 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,8 +1,8 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++){
-            int sumer = target - nums[i];
-            int sumer_index = linear_find(nums,sumer,i+1);
+            int sumer = target - nums[i]; // get the difference
+            int sumer_index = linear_find(nums,sumer,i+1); // check if that value exist in the array or not using simple linear search.
             if(sumer_index==-1){
                 continue;
             }
@@ -10,6 +10,7 @@ class Solution {
         }
     return new int[0];
     }
+    
     public int linear_find(int[] nums,int x,int start){
         for(int i=start; i< nums.length;i++){
             if(nums[i]==x){ 
